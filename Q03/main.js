c4b3c7efdba9b041a6efe52ed955da67014c5795
Rama-Qandeel <rama.qndeel@gmail.com>
2020-11-20 23:12:04 +0200

    solve q3 in 2 minutes

--- Q03/main.js
+++ Q03/main.js
@@ -6,8 +6,13 @@ console.log('Problem Solving Q: 3 ');
 Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
 */
 
-function firstReverse(str) {
-  // YOUR CODE HERE
+function firstReverse(str) {  
+ let strReverse=""
+  for(i=str.length-1;i>=0;i--)
+  {
+strReverse+=str[i]
+  }
+  return strReverse;
 }
 
 /* 
