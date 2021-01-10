46fc6873cbf99f1b115093c7c3ad929194d47a4e
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-10 16:10:39 +0200

    solve q7

--- 02_Data Structures + Algorithms [27 Q]/exercises/07_anagrams/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/07_anagrams/index.js
@@ -8,6 +8,34 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+//input 2 string
+/*
+1-remove spaces or punctuation
+2-cheak lenght of 2 string
+3-check the char 
+*/
+//output boolean
+var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ " "]/g;
+stringA=stringA.replace(regex,"").toUpperCase().split("")
+stringB=stringB.replace(regex,"").toUpperCase().split("")
+let isAnagrams=true
+// console.log('stringt',stringA);
+// console.log('stringt',stringB);
+if(stringA.length===stringB.length){
+   stringA.forEach((char)=>{
+     if(stringB.indexOf(char)===-1){
+        isAnagrams=false
+     } 
+   })
+}else{
+    isAnagrams=false
+   }
+return isAnagrams
+}
+
+
+
+
 
 module.exports = anagrams;
