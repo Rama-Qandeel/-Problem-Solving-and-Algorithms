ad60f65f567363a8db636393bd25de2e170184c1
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-13 15:59:47 +0200

    solve q16

--- 01_Problem Solving [26 Q]/Q16/main.js
+++ 01_Problem Solving [26 Q]/Q16/main.js
@@ -7,7 +7,18 @@ Find the first item that appear an even number of times in an array.
 */
 
 function evenAppearance() {
-  // YOUR CODE HERE
+  var storage = {};
+  array.forEach(function (value, index) {
+    storage[value] = storage[value] + 1 || 1;
+  });
+  for (var i = 0; i < array.length; i++) {
+    var current = array[i];
+
+    if (storage[current] % 2 === 0) {
+      return current;
+    }
+  }
+  return null;
 }
 
 /* 
