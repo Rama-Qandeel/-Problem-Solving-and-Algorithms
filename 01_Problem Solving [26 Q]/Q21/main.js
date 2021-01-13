7210bce62c163fa47c030b96b62ba6c324df8eac
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-13 15:36:57 +0200

    solve q21

--- 01_Problem Solving [26 Q]/Q21/main.js
+++ 01_Problem Solving [26 Q]/Q21/main.js
@@ -6,8 +6,22 @@ console.log('Problem Solving Q: 21');
 Given a string, delete any characters that are not unique from the string.
 */
 
-function removeNotUnique() {
-  // YOUR CODE HERE
+function removeNotUnique(str) {
+  let result=""
+let obj={}
+str.split("").forEach(element => {
+  if(obj[element]){
+    obj[element]+=1
+  }else{
+    obj[element]=1
+  }
+})
+console.log(obj);
+
+for(const char in obj){
+  if(obj[char]===1)  result+=char
+}
+return result
 }
 
 /* 
