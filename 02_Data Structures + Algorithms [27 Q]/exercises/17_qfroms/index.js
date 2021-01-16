05b7e33a14a2ee41a7c0b6ff4b47f046934ed0d4
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-16 21:08:06 +0200

    solve q17 frome data structures

--- 02_Data Structures + Algorithms [27 Q]/exercises/17_qfroms/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/17_qfroms/index.js
@@ -14,6 +14,43 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+
+class Queue {
+    constructor(){
+      this.s1=new Stack();
+      this.s2=new Stack();
+    }
+    
+    add(value) {
+        this.s1.push(value);
+
+      }
+      remove() {
+        while (this.s1.peek()) {
+          this.s2.push(this.s1.pop());
+          
+        }
+      const value=this.s2.pop()
+        
+      while (this.s2.peek()) {
+            this.s1.push(this.s2.pop());
+          }
+
+        return value
+    }
+    peek(){
+        while (this.s1.peek()) {
+            this.s2.push(this.s1.pop());
+            
+          }
+          const value=this.s2.peek()
+          while (this.s2.peek()) {
+                this.s1.push(this.s2.pop());
+              }
+              return value
+    }
+
+}
+
 
 module.exports = Queue;
