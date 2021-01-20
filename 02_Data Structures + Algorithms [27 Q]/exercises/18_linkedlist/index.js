d7a22a7c85cae5dede153c2f0690b8ef30463237
Rama-Qandeel <rama.qndeel@gmail.com>
2021-01-20 10:12:55 +0200

    solve q18

--- 02_Data Structures + Algorithms [27 Q]/exercises/18_linkedlist/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/18_linkedlist/index.js
@@ -2,8 +2,43 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+      }
+}
 
-class LinkedList {}
+class LinkedList {
+constructor(head = null) {
+    this.head = head;
+      }
+
+insertFirst(data) {
+ const node = new Node(data, this.head);
+    this.head = node;
+      }
+      size() {
+        if (this.head) {
+          let node = this.head;
+          let size = 1;
+          while (node.next) {
+            node = node.next;
+            size++;
+          }
+          return size;
+        }
+        return 0;
+      }
+      getFirst() {
+        return this.head;
+      }
+     removeFirst() {
+        if (!this.head) {
+          return;
+        }
+        this.head = this.head.next;
+      }
+}
 
 module.exports = { Node, LinkedList };
